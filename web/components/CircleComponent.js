24be6ad9413e67c174e22af43fcd12e9c8ee88c3
Ttommeke <tomvalkeneers@hotmail.com>
2018-01-02 15:31:46 +0100

    circle images

--- web/components/CircleComponent.js
+++ web/components/CircleComponent.js
@@ -6,8 +6,13 @@ Vue.component('circle-component', {
     </div>
     <div class="box-body" id="circleBoxBody">
         <svg v-bind:height="width" v-bind:width="width">
+            <defs>
+                <pattern v-for="i in persons" v-bind:id="imageIds[i]" v-bind:x="xCoordinates[i] - sizeUserCircles" v-bind:y="yCoordinates[i] - sizeUserCircles" patternUnits="userSpaceOnUse" v-bind:width="sizeUserCircles*2" v-bind:height="sizeUserCircles*2">
+                    <image x="0" y="0" v-bind:xlink:href="images[i]" v-bind:width="sizeUserCircles*2" v-bind:height="sizeUserCircles*2"></image>
+                </pattern>
+            </defs>
             <circle v-bind:cx="width/2" v-bind:cy="width/2" v-bind:r="(width/2)*widthFactorInnerRing" stroke="green" stroke-width="8" fill="white" />
-            <circle v-for="i in persons" stroke="#CCC" v-bind:cx="xCoordinates[i]" v-bind:cy="yCoordinates[i]" v-bind:r="(width/2)*sizeFactorPhotos*widthFactorInnerRing/5" stroke-width="6" fill="black" />
+            <circle v-for="i in persons" stroke="#CCC" v-bind:cx="xCoordinates[i]" v-bind:cy="yCoordinates[i]" v-bind:r="sizeUserCircles" stroke-width="6" v-bind:fill="'url(#person' + i + ')'" />
         </svg>
     </div>
 </div>
@@ -22,6 +27,7 @@ Vue.component('circle-component', {
             widthFactorInnerRing: 0.7,
             sizeFactorPhotos: 2,
             images: [
+                    "none",
                     "web/img/user1-128x128.jpg",
                     "web/img/user8-128x128.jpg",
                     "web/img/user7-128x128.jpg",
@@ -36,16 +42,17 @@ Vue.component('circle-component', {
         this.width = $("#circleBoxBody").width();
     },
     computed: {
+        sizeUserCircles: function() {
+            return (this.width/2)*this.sizeFactorPhotos*this.widthFactorInnerRing/5;
+        },
         xCoordinates: function () {
             var xCoor = [0];
 
             var innerWidth = this.widthFactorInnerRing * this.width/2;
 
-            for (var i = 0; i < this.persons; i++) {
-                xCoor.push((Math.cos((Math.PI*2/this.persons)*i + Math.PI*3/2) * innerWidth + this.width/2));
+            for (var i = 1; i <= this.persons; i++) {
+                xCoor.push((Math.cos((Math.PI*2/this.persons)*(i - 1) + Math.PI*3/2) * innerWidth + this.width/2));
             }
-
-            console.log(xCoor);
 
             return xCoor;
         },
@@ -54,13 +61,20 @@ Vue.component('circle-component', {
 
             var innerWidth = this.widthFactorInnerRing * this.width/2;
 
-            for (var i = 0; i < this.persons; i++) {
-                yCoor.push(Math.sin((Math.PI*2/this.persons)*i + Math.PI*3/2) * innerWidth + this.width/2);
+            for (var i = 1; i <= this.persons; i++) {
+                yCoor.push(Math.sin((Math.PI*2/this.persons)*(i - 1) + Math.PI*3/2) * innerWidth + this.width/2);
             }
 
-            console.log(yCoor);
-
             return yCoor;
+        },
+        imageIds: function () {
+            var ids = ["none"];
+
+            for (var i = 1; i <= this.persons; i++) {
+                ids.push("person" + i);
+            }
+
+            return ids;
         },
     },
     methods: {
